0b22dcade04cce48f81a127322dcb0412e3e0248
Jared Seefried <jared.seefried@yahoo.com>
2020-11-22 20:44:15 -0700

    Created the .map method to the employees.json file.

---
+++ src/components/EmployeeContainer.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import EmployeeDetail from './EmployeeDetail'
+// import axios from 'axios'
+import employees from '../employees.json'
+
+
+class Employees extends Component {
+
+    state = {
+        employees
+    }
+
+    render() {
+        return (
+            <div className="col-12">
+                <div className="row">
+                    <ul className="list-group col-12">
+                        {this.state.employees.map(employee => (
+                            <EmployeeDetail
+                                id={employee.id}
+                                key={employee.id}
+                                name={employee.name}
+                                image={employee.image}
+                                department={employee.department}
+                                email={employee.email}
+                                phone={employee.phone} />
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Employees
